53b972acfad0b33b5f251fdf0621428c4ac2e553
lefinoe <lefinoe@gmail.com>
2022-11-16 00:49:15 -0300

    GX-2316 avances

---
+++ cypress/e2e/Tests/Suites/GX-2316/Datepicket.buscardestino.cy.js
@@ -0,0 +1,12 @@
+describe("SpaceBeyond | Datepicker | Buscar destino por fecha y grupo de pasajeros", () =>
+{
+    beforeEach("Precondición: Usuario debe estar situado en el homepage", () =>
+    {
+        cy.visit("https://demo.testim.io/")
+        cy.url().should("contain","login")
+    })
+    it("2316|TS 2317| TC01: Verificar que usuario pueda buscar destino solo por fecha valida de partida y retorno", () =>
+    {
+        cy.get
+    })
+})
