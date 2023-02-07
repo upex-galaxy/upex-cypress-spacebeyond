18064c16566eb33225165258d27146c97a057eb2
Rafael Mejia S (ClaroDom) <Rafael_Mejia_S@claro.com.do>
2023-02-06 23:07:04 -0400

    Adding US-GX2-652 files

---
+++ cypress/e2e/Tests/Booking/GX2-652-DestinationCheckout.cy.js
@@ -0,0 +1,11 @@
+describe("US-GX2-652: SpaceBeyond | Booking | Book a Destination in Checkout",()=>{
+
+    beforeEach("Predondicion:",()=>{
+    
+    })
+    
+    it("US-GX2-652 | TS-GX2-653 | TC1: Realizar checkout de destino exitoso (Positivo)",()=>{
+            //Pasos de tus casos de pruebas
+    })
+    
+    })
