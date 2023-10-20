14e2a026da8876e9a0880ef39bc18dee27873657
PabloCz <Pablotest@gmail.com>
2023-10-20 17:40:11 -0300

    GX2-8384 Config inicial , add property testExecKey on CI-Suite

---
+++ cypress/e2e/Tests/DatePicker/GX2-8384-Datapicker.cy.js
@@ -0,0 +1,5 @@
+describe('SpaceBeyond | Datepicker | Buscar destino por fecha y grupo de pasajeros', () => {
+	beforeEach('PRC: usuario está situado en el home del site "Space&Beyond"', () => {
+		cy.visit('/')
+	})
+})
