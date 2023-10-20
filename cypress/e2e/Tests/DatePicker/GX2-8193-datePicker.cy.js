eeaf5c03b5a13f10e75d20f3ab686a6c0667920a
rafo18 <rafaelbarreto10@hotmail.com>
2023-10-20 17:54:36 -0400

    GX2-8193 Se realizo el POM y se empezo a trabajar en los metodos

--- cypress/e2e/Tests/DatePicker/GX2-8193-datePicker.cy.js
+++ cypress/e2e/Tests/DatePicker/GX2-8193-datePicker.cy.js
@@ -4,9 +4,10 @@ import { homePage } from '@pages/GX2-8193-datePicker.page'
 describe('GX2-8193-SpaceBeyond | Datepicker | Buscar destino por fecha y grupo de pasajeros', () => {
 	beforeEach(() => {
 		cy.visit('/')
+		cy.url().should('contain', 'demo.testim.io/')
 	})
-	it('sdfsd', () => {
-		cy.get('a')
+	it('asd', () => {
+		homePage.selectDeparting()
 	})
 })
 
--- cypress/support/pages/GX2-8193-datePicker.page.js
+++ cypress/support/pages/GX2-8193-datePicker.page.js
@@ -1,9 +1,32 @@
 class HomePage {
 	get = {
-		departingPicker: cy.get('[class ^= theme__input] input').eq(0),
-		returningPicker: cy.get('[class ^= theme__input] input').eq(1),
-		adultsPicker: cy.get('[class ^= theme__input] input').eq(2),
-		childrenPicker: cy.get('[class ^= theme__input] input').eq(3),
+		departingPicker: () => cy.get('[class ^= theme__input] input').eq(0),
+		returningPicker: () => cy.get('[class ^= theme__input] input').eq(1),
+		adultsPicker: () => cy.get('[class ^= theme__input] input').eq(2),
+		childrenPicker: () => cy.get('[class ^= theme__input] input').eq(3),
+		containerPicker: () => cy.get('.Hero__form-box___126DY'),
+
+		//Pop up calendar
+		popUpCalendar: () => cy.get('[class ^= theme__dialog]'),
+		calendarDays: () => cy.get('[class= theme__day___3cb3g] span').and('be.visible'),
+		currentDaysMonth: () => cy.get('[class^= theme__day]').not('[class$=theme__disabled___2N4Gy]'),
+		rightButton: () => cy.get('[id=right]'),
+		leftButton: () => cy.get('[id=left]'),
+	}
+
+	selectDeparting() {
+		const randomMonth = 4
+		cy.log(randomMonth)
+		this.get.departingPicker().click()
+		this.get.popUpCalendar().should('be.visible')
+		for (let i = 0; i <= randomMonth - 1; i++) {
+			this.get.rightButton().click()
+		}
+		cy.wait(800)
+		this.get.calendarDays().then((index) => {
+			console.log(index)
+			cy.log(index.length)
+		})
 	}
 }
 
