adfe3218ddc406eaf012ef3f2e55ce13611cedc2
AlexanderWindecker <waa_alex@hotmail.com>
2024-09-30 14:23:00 -0300

    modifique regresion y sanity del workflow. Se eliminaron test viejos.

--- cypress/e2e/Tests/Accounts/GX-6230-LoginLogout.cy.js
+++ cypress/e2e/Tests/Accounts/GX-6230-LoginLogout.cy.js
@@ -4,18 +4,21 @@ describe('SpaceBeyond | Account | Log-In and Log-Out', () => {
 		cy.contains('Log in').click()
 		cy.url().should('contain', 'login')
 	})
-	it('US-GX-6230| TC1:  Validar Login correctamente con credenciales válidas', () => {
+	it.only('US-GX-6230| TC1:  Validar Login correctamente con credenciales válidas', () => {
 		//Validar Login Exitoso
-		cy.loginSpace('Franco', 'asd123')
+		cy.get('#login > :nth-child(1) > .theme__inputElement___27dyY').type('Franco')
+		cy.get('#login > :nth-child(2) > .theme__inputElement___27dyY').type('asd123')
+		cy.get('.Login__button-box___1OzjH > .theme__raised___ONZv6').click()
+		// cy.loginSpace('Franco', 'asd123')
 	})
 	it('US-GX-6230| TC2:  Validar No iniciar sesión con el campo Name vacío', () => {
-		cy.loginSpace('', 'asd123')
+		// cy.loginSpace('', 'asd123')
 		//Validate Login
 		cy.contains('Name is a required field.').should('be.visible')
 		cy.contains('Password is a required field.').should('not.exist')
 	})
 	it('US-GX-6230| TC3:  Validar No iniciar sesión con el campo Password vacío', () => {
-		cy.loginSpace('Franco', '')
+		// cy.loginSpace('Franco', '')
 		//Validate Login
 		cy.contains('Password is a required field.').should('be.visible')
 		cy.contains('Name is a required field.').should('not.exist')
@@ -23,7 +26,7 @@ describe('SpaceBeyond | Account | Log-In and Log-Out', () => {
 	it('US-GX-6230| TC4:  Validar LogOut correctamente luego de haber iniciado sesión', () => {
 		//Precondición
 
-		cy.loginSpace('Franco', 'asd123')
+		// cy.loginSpace('Franco', 'asd123')
 		//Acción
 		cy.get('.mui-dropdown button').click()
 		cy.get('.mui--is-open a').click()
