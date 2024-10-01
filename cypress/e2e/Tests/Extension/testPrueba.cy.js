143f2de2757f3784fc075bbfa6d5e7ea98233031
AlexanderWindecker <waa_alex@hotmail.com>
2024-10-01 16:06:28 -0300

    agregue un test de prueba para correr smoke

--- cypress/e2e/Tests/Extension/testPrueba.cy.js
+++ cypress/e2e/Tests/Extension/testPrueba.cy.js
@@ -1,8 +1,8 @@
-describe('', () => {
+describe('Visitar la pagina Space & Beyonce', () => {
 	beforeEach('PCR: Visitar la pagina',() => {
 		cy.visit('https://demo.testim.io/')
 	});
-	it('', () => {
+	it('Visitar y validar', () => {
 		cy.get('.Gallery__headline-1___2lHj5').should('have.text', 'Your next destination')
 	});
 });
